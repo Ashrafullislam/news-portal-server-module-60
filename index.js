139c7c6ace1e4372399b1446fa45ba508590ee51
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-10-20 18:28:50 +0600

    added the news.json data from localhost express server run

--- index.js
+++ index.js
@@ -3,52 +3,27 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000 ;
 app.use(cors())
-const categories = ([
-    {
-      "id": "01",
-      "name": "Breaking News"
-    },
-    {
-      "id": "02",
-      "name": "Regular News"
-    },
-    {
-      "id": "03",
-      "name": "International News"
-    },
-    {
-      "id": "04",
-      "name": "Sports"
-    },
-    {
-      "id": "05",
-      "name": "Entertainment"
-    },
-    {
-      "id": "06",
-      "name": "Culture"
-    },
-    {
-      "id": "07",
-      "name": "Arts"
-    },
-    {
-      "id": "08",
-      "name": "All News"
-    }
-  ])
 
-  const news = ('./news.json')
+
+
+// all news category 
+const categories = require ('./data/categories.json')
+
+  // all news data 
+const newsData = require ('./data/news.json')
+// root folder for test server running 
 app.get('/', (req,res) => {
-    res.send('News api server running')
+  res.send('News Category runnin on server ')
 })
 
+
+// all news category  display 
 app.get('/categories-news', (req,res)=>{
     res.send(categories)
 })
 
-app.get('/news-portal',(req,res)=> {
-    res.send(news)
+app.get('/all-news', (req,res) => {
+  res.send(newsData)
 })
 
 app.listen(port, ()=> {
